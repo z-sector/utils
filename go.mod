49ef8b13737b7b906d2185ab97d644bc55ff5675
George Ramaniuk <george.ramaniuk@gmail.com>
2022-10-21 00:12:31 +0300

    new version

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/z-sector/utils/v2
+module github.com/z-sector/utils/v3
 
 go 1.18
--- utils.go
+++ utils.go
@@ -1,6 +1,6 @@
 package utils
 
-func InSlice(a []string, x string) bool {
+func InSliceInt(a []string, x string) bool {
 	for _, n := range a {
 		if x == n {
 			return true
